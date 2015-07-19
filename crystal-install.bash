de2ed7a9670c3f57a313139aafd2668918904a8f
Pavel Korotkov <pkorotkov@gmail.com>
2015-07-19 03:33:29 +0300

    Update crystal-install.bash

--- crystal-install.bash
+++ crystal-install.bash
@@ -10,4 +10,4 @@ apt-get install -y make
 wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key|sudo apt-key add -
 # Set LLVM bound to 3.5 release.
 apt-get install -y clang-3.5 lldb-3.5
-apt-get install -y libz-dev libedit-dev libssl-dev libevent-dev libpcre3-dev libgc-dev
+apt-get install -y libz-dev libedit-dev libssl-dev libevent-dev libpcre3-dev libgc-dev libpcl1-dev libunwind-dev
