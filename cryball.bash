42e9e03c8055e1aa01cf89baf1b589c9867425e4
Pavel Korotkov <pkorotkov@gmail.com>
2016-05-18 02:07:29 +0300

    Update cryball.bash

--- cryball.bash
+++ cryball.bash
@@ -5,7 +5,7 @@ echoerr() {
 }
 
 # Check if at least two arguments are passed.
-if [ "$#" -lt 2 ]; then
+if [ "$#" -lt 1 ]; then
     echoerr "error: incorrect list of arguments"
     exit 1
 fi
@@ -21,17 +21,19 @@ case "$1" in
         sudo apt-get install -y make g++ llvm-3.6 libedit-dev lib32z1-dev
         # Clone the latest codebase snapshot.
         git clone https://github.com/crystal-lang/crystal.git $2
-        # Build the snapshot of crystal compiler.
+        # Go to installation directory.
         cd $2
+        # Build the snapshot of crystal compiler.
         export LIBRARY_PATH=${EMBEDDED_LIB_PATH}
         make
         # Create alias for snapshot crystal compiler.
         SCRIPT_DIR_PATH=$( cd $(dirname $0) ; pwd -P )
         SCRIPT_NAME=`basename $0`
+        echo "export SHCRYSTAL_DIR_PATH=$2" >> $HOME/.bashrc
         echo "alias shcrystal=\"${SCRIPT_DIR_PATH}/${SCRIPT_NAME} execute $2\"" >> $HOME/.bashrc
     ;;
     "update")
-        cd $2
+        cd ${SHCRYSTAL_DIR_PATH}
         # Check if there have been updates that arrived from repository.
         git pull | grep "Already up-to-date."
         if [[ ! $? -eq 0 ]]; then
@@ -41,7 +43,7 @@ case "$1" in
     ;;
     "execute")
         export LIBRARY_PATH=${EMBEDDED_LIB_PATH}
-        $2/.build/crystal "${@:3}"
+        ${SHCRYSTAL_DIR_PATH}/.build/crystal "${@:2}"
     ;;
     *)
         echoerr "error: unknown command"
