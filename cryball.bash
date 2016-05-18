2202974d602beb88e464f531db4900c285f4ad68
Pavel Korotkov <pkorotkov@gmail.com>
2016-05-18 03:37:47 +0300

    Update cryball.bash

--- cryball.bash
+++ cryball.bash
@@ -21,13 +21,13 @@ case "$1" in
         sudo apt-get install -y make g++ llvm-3.6 libedit-dev lib32z1-dev
         # Clone the latest codebase snapshot.
         git clone https://github.com/crystal-lang/crystal.git $2
+        SCRIPT_DIR_PATH=$( cd $(dirname $0) ; pwd -P )
+        SCRIPT_NAME=`basename $0`
         # Go to the installation directory.
         cd $2
         # Build a snapshot of the crystal compiler.
         export LIBRARY_PATH=${EMBEDDED_LIB_PATH}
         make
-        SCRIPT_DIR_PATH=$( cd $(dirname $0) ; pwd -P )
-        SCRIPT_NAME=`basename $0`
         # TODO: Think about the file (now - $HOME/.bachrc) where env variables and aliases should be written in.
         ENV_FILE=${HOME}/.bashrc
         # Delete old env lines (which contain strings 'SHCRYSTAL_DIR_PATH' or 'shcrystal') from ENV_FILE.
